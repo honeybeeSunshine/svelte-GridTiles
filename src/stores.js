8407cf1d8bd9853cfca9bc09c31c763d8ae931cf
honeybeeSunshine <98979350+honeybeeSunshine@users.noreply.github.com>
2022-02-03 14:20:56 -0500

    Add files via upload

---
+++ src/stores.js
@@ -0,0 +1,8 @@
+import { writable } from 'svelte/store';
+
+export let unlockTiles = writable(false);
+export let showDrops = writable(false);
+export let activeTile = writable(null);
+export let dropTarget = writable(null);
+
+export let currentLayout = writable([]);
